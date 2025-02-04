b73b63c87f7b83f794aa7b5f800c8e6720799115
Megan Chiu <megan.chiu.2014@gmail.com>
2025-02-04 15:15:46 -0800

    fetch data and useeffect

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { useEffect } from "react";
 import ContactRow from "./ContactRow";
 
 const dummyContacts = [
@@ -10,6 +11,20 @@ const dummyContacts = [
 
 function ContactList() {
   const [contacts, setContacts] = useState(dummyContacts)
+  useEffect(()=>{
+    // Async function to fetch the data from the API
+    async function fetchContacts() {
+      try {
+        const response = await fetch('https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users');
+        const data = await response.json();
+        console.log("fetched contacts => ", data)
+        setContacts(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchContacts();
+  },[])
 
   // console.log("Contacts:", contacts);
 
